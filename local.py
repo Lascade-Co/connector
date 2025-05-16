73b158c23491c45a83e89f2fd1b8cc2754434bb2
Rohit T P <tprohit9@gmail.com>
2025-05-17 05:14:01 +0530

    add local.py for scheduled execution of pg_replication_pipeline; include logging and error handling

---
+++ local.py
@@ -0,0 +1,37 @@
+import logging
+import sys
+import time
+
+import schedule
+
+from pipelines import pg
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(levelname)s │ %(message)s",
+    handlers=[
+        logging.StreamHandler(sys.stdout),
+        logging.FileHandler("pipeline.log", mode="a")
+    ]
+)
+
+def safe_pg_pipeline():
+    """Run the pg_replication_pipeline safely."""
+    # noinspection PyBroadException
+    try:
+        pg.run()
+    except Exception:
+        logging.exception(f"Error in pg_replication_pipeline", exc_info=True)
+
+
+def main():
+    schedule.every(1).hours.do(safe_pg_pipeline)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(60)
+
+
+if __name__ == "__main__":
+    logging.info("Starting pipelines...")
+    main()
