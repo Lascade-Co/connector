c475fb1e603c0d558da6415fa63e62bf88e09024
Rohit <rohittp@Rohits-MacBook-Pro.local>
2026-01-19 09:05:09 +0530

    Expand `TABLE_TO_FIELD_MAPPING` to include additional tables and their corresponding fields.

--- pipelines/pg/dashboard/constants.py
+++ pipelines/pg/dashboard/constants.py
@@ -1,14 +1,22 @@
 # Maps each table to the field used to track the last pulled record.
 TABLE_TO_FIELD_MAPPING = {
-    "reports_report": "updated_at",
-    "users_application": "updated_at",
-    "users_appuser": "updated_at",
-    "users_conversions": "updated_at",
-    "users_deviceuser": "id",
-    "users_provider": "updated_at",
-    "users_providerservice": "updated_at",
-    "users_usersession": "id",
-    "users_attribution": "id"
+    "auth_user": "id",
+    "common_app": "updated_at",
+    "common_coupon": "updated_at",
+    "common_trackablepayload": "created_at",
+    "sso_appinstance": "updated_at",
+    "travel_animator_animationstate": "updated_at",
+    "travel_animator_annotation": "id",
+    "travel_animator_annotationmedia": "id",
+    "travel_animator_glbfile": "id",
+    "travel_animator_map": "updated_at",
+    "travel_animator_marker": "id",
+    "travel_animator_routepoint": "updated_at",
+    "travel_animator_savedroute": "updated_at",
+    "travel_animator_texture": "updated_at",
+    "travel_animator_threedmodel": "updated_at",
+    "travel_animator_threedmodel_allowed_users": "id",
+    "travel_animator_usdzfile": "id"
 }
 
 SELECTED_TABLES = list(TABLE_TO_FIELD_MAPPING.keys())
